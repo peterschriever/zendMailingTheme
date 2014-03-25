f9265a56fdb301b49a9c9b561153aa3665ef0aa1
Peter Schriever <peterzen72@gmail.com>
2014-03-25 13:42:02 +0100

    form voor een nieuwe mail toegevoegt

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,6 +13,33 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
+    public function newMailAction()
+    {
+        /*$form = new Application_Form_Content();
+        $form->submit->setLabel('Toevoegen');
+        $this->view->form = $form;
+        if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if($form->isValid($formData)) {
+                $title = $form->getValue('title');
+                $content = $form->getValue('content');
+                $created = $form->getValue('created');
+                $updated = $form->getValue('updated');
+
+                $courses = new Application_Model_dbTable_Content();
+                $courses->addContent($title, $content, $created, $updated);
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        }*/
+        $form = new Application_Form_Mail();
+        //$this->view->form
+
+    }
+
 
 }
+
+
 
--- application/forms/Mail.php
+++ application/forms/Mail.php
@@ -5,7 +5,48 @@ class Application_Form_Mail extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setName("mail");
+
+        $name = new Zend_Form_Element_Text('name');
+        $name->setLabel('Naam: ')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        $subject = new Zend_Form_Element_Text('subject');
+        $subject->setLabel('Onderwerp: ')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        $theme = new Application_Form_Element_ThemeSelect('themeSelect');
+        $theme->setLabel('Thema: ')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        $newsletter = new Zend_Form_Element_Checkbox('newsletter');
+        $newsletter->setLabel('Nieuwsbrief: ')
+            ->setRequired(true)
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim');
+
+        $content = new Zend_Form_Element_Textarea('message');
+        $content->setLabel('Voer uw bericht hier in:')
+            ->setRequired(true)
+            ->setAttrib('cols','30')
+            ->setAttrib('rows', '5')
+            ->addFilter('StripTags')
+            ->addFilter('StringTrim')
+            ->addValidator('NotEmpty');
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setAttrib('id', 'sendMail');
+
+        $this->addElements(array($name, $subject, $theme, $content, $submit));
     }
 
 
