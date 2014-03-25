9d6026637a6360d03e5e7958a011afd0ac585d7f
Peter Schriever <peterzen72@gmail.com>
2014-03-25 13:57:13 +0100

    indexcontroller uitgebreid met actie voor een nieuwe mail, actie maakt gebruik van het mail formulier

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,13 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initSetupBaseUrl() {
+        $this->bootstrap('frontcontroller');
+        $controller = Zend_Controller_Front::getInstance();
+        $controller->setBaseUrl('/zendThemeMail/public');
+        ini_set("error_reporting", E_ALL);
+        ini_set("display_errors", "On");
+    }
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -15,7 +15,10 @@ class IndexController extends Zend_Controller_Action
 
     public function newMailAction()
     {
-        /*$form = new Application_Form_Content();
+        /*
+         * MyCMS voorbeeld(van het zend boekje)
+         *
+        $form = new Application_Form_Content();
         $form->submit->setLabel('Toevoegen');
         $this->view->form = $form;
         if($this->getRequest()->isPost()) {
@@ -26,15 +29,24 @@ class IndexController extends Zend_Controller_Action
                 $created = $form->getValue('created');
                 $updated = $form->getValue('updated');
 
-                $courses = new Application_Model_dbTable_Content();
-                $courses->addContent($title, $content, $created, $updated);
+                $contentmodel = new Application_Model_dbTable_Content();
+                $contentmodel->addContent($title, $content, $created, $updated);
                 $this->_helper->redirector('index');
             } else {
                 $form->populate($formData);
             }
         }*/
         $form = new Application_Form_Mail();
-        //$this->view->form
+        $this->view->form = $form;
+        if($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if($form->isValid($formData)) {
+                // TODO: functie schrijven om mails uiteindelijk te versturen
+                // mailing form is valid => send mail function parameters are in $form->getValue('paramname')
+            } else {
+                $form->populate($formData);
+            }
+        }
 
     }
 
